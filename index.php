a90a7ca1fcc06c149ad5bb8e3607101d52292d3e
jhonD853 <natsusa853@gmail.com>
2024-02-07 22:26:24 -0500

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-    <script src="js/scri.js"></script>
+    <script src="js/scri.js">
+</script>
 </head>
 <body>
     <div id="contenedor">
